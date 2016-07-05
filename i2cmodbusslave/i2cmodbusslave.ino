59879c65f5a71f0f5a6565af7f4d2eec319a211c
Art Navsegda <artnavsegda@gmail.com>
2016-07-05 23:11:49 +0300

    16 bit wide

--- i2cmodbusslave/i2cmodbusslave.ino
+++ i2cmodbusslave/i2cmodbusslave.ino
@@ -5,14 +5,20 @@
 #include <Modbus.h>
 #include <ModbusIP_ENC28J60.h>
 
+#define MSB(u16) (((uint8_t* )&u16)[1])
+#define LSB(u16) (((uint8_t* )&u16)[0])
+
 ModbusIP mb;
 int memaddr;
 int firstbyte;
+int secondbyte;
+int regdata;
 
 void setup() {
   // put your setup code here, to run once:
   Wire.begin(8);
   Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
   byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
   byte ip[] = { 192, 168, 1, 120 };
   mb.config(mac, ip);
@@ -30,7 +36,26 @@ void receiveEvent(int howMany) {
   }
   if (Wire.available()) {
     firstbyte = Wire.read();
+  }
+  if (Wire.available()) {
+    secondbyte = Wire.read();
+  }
+  if (howMany == 2)
+  {
     mb.Hreg(memaddr,firstbyte);
   }
+  if (howMany == 3)
+  {
+    LSB(regdata) = firstbyte;
+    MSB(regdata) = secondbyte;
+    mb.Hreg(memaddr,regdata);
+  }
+}
+
+void requestEvent() {
+  //Wire.write(mb.Hreg(memaddr));
+  regdata = mb.Hreg(memaddr);
+  Wire.write(LSB(regdata));
+  Wire.write(MSB(regdata));
 }
 
