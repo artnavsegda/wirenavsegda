4a72b706da2a27a5509ac9eeb4083ba228a9eb01
Art Navsegda <artnavsegda@gmail.com>
2016-07-06 18:57:07 +0300

    klutz

--- i2cmodbusslave/i2cmodbusslave.ino
+++ i2cmodbusslave/i2cmodbusslave.ino
@@ -22,8 +22,20 @@ void setup() {
   byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
   byte ip[] = { 192, 168, 1, 120 };
   mb.config(mac, ip);
-  mb.addHreg(10);//total
-  mb.addHreg(11);//total
+  mb.addHreg(8);//code of current mode
+  mb.addHreg(10); mb.addHreg(11);//total
+  mb.addHreg(22); mb.addHreg(23);//temp
+  mb.addHreg(28);//errors and warnings
+  mb.addCoil(1,true);//status of spectrometer
+  mb.addCoil(2,true);//status of thermocontrollers
+  mb.addCoil(3,true);//availibility for external request
+  mb.addCoil(4,false);//status of zero test
+  mb.addCoil(5,false);//status of calibration
+  mb.addCoil(99,false);//run calibration
+  mb.addCoil(100,false);//run zero test
+  mb.addCoil(101,false);//run elemental
+  mb.addCoil(102,false);//start purge
+  mb.addCoil(103,false);//end purge
 }
 
 void loop() {
@@ -32,21 +44,15 @@ void loop() {
 }
 
 void receiveEvent(int howMany) {
-  if (Wire.available()) {
+  if (Wire.available())
     memaddr = Wire.read();
-  }
-  if (Wire.available()) {
+  if (Wire.available())
     firstbyte = Wire.read();
-  }
-  if (Wire.available()) {
+  if (Wire.available())
     secondbyte = Wire.read();
-  }
   if (howMany == 2)
-  {
-    mb.Hreg(memaddr,firstbyte);
-  }
-  if (howMany == 3)
-  {
+    mb.Coil(memaddr,firstbyte);
+  if (howMany == 3) {
     LSB(regdata) = firstbyte;
     MSB(regdata) = secondbyte;
     mb.Hreg(memaddr,regdata);
@@ -55,8 +61,14 @@ void receiveEvent(int howMany) {
 
 void requestEvent() {
   uint8_t buf[2];
-  buf[0] = mb.Hreg(memaddr);
-  buf[1] = mb.Hreg(memaddr)>>8;
-  Wire.write(buf,2);
+  uint16_t replymessage;
+  if ((memaddr >= 99)||(memaddr<8))
+    Wire.write(mb.Coil(memaddr));
+  else {
+    replymessage = mb.Hreg(memaddr);
+    buf[0] = LSB(replymessage);
+    buf[1] = MSB(replymessage);
+    Wire.write(buf,2);
+  }
 }
 
