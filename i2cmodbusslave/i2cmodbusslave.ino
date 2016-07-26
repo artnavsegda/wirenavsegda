c8fdac528cd6ad5f812d0e86ed8f33c658da9ff8
Art Navsegda <artnavsegda@gmail.com>
2016-07-26 15:01:44 +0300

    eprom

--- i2cmodbusslave/i2cmodbusslave.ino
+++ i2cmodbusslave/i2cmodbusslave.ino
@@ -1,3 +1,4 @@
+#include <EEPROM.h>
 #include <Wire.h>
 #include <enc28j60.h>
 #include <EtherCard.h>
@@ -5,14 +6,96 @@
 #include <Modbus.h>
 #include <ModbusIP_ENC28J60.h>
 
+float STARTCODE=5.0;
+float STARTTOTAL=1.0;
+float STARTOXIDIZED=1.0;
+float STARTMONITOR=1.0;
+float STARTVACUUM=1.0;
+float STARTDILUTION=1.0;
+float STARTBYPASS=1.0;
+float STARTTEMP=1.0;
+float STARTELEMENTAL=1.0;
+float STARTERROR=1.0;
+float STARTCOEFF=1.0;
+
 #define MSB(u16) (((uint8_t* )&u16)[1])
 #define LSB(u16) (((uint8_t* )&u16)[0])
+
+#define  MSW(u32)          (((uint16_t*)&u32)[1])
+#define  LSW(u32)          (((uint16_t*)&u32)[0])
 
 ModbusIP mb;
 int memaddr;
 int firstbyte;
 int secondbyte;
 int regdata;
+
+void setcoils(int start, int amount)
+{
+  for (int i = start; i < amount; i++)
+    mb.addCoil(i);
+}
+
+void sethregs(int start, int amount)
+{
+  for (int i = start; i < amount; i++)
+    mb.addHreg(i);
+}
+
+void savepermanent(void)
+{
+  LSW(STARTCODE) = mb.Hreg(8); MSW(STARTCODE) = mb.Hreg(9); EEPROM.put(100, STARTCODE);
+  LSW(STARTTOTAL) = mb.Hreg(10); MSW(STARTTOTAL) = mb.Hreg(11); EEPROM.put(104, STARTTOTAL);
+  LSW(STARTOXIDIZED) = mb.Hreg(12); MSW(STARTOXIDIZED) = mb.Hreg(13); EEPROM.put(108, STARTOXIDIZED);
+  LSW(STARTMONITOR) = mb.Hreg(14); MSW(STARTMONITOR) = mb.Hreg(15); EEPROM.put(112, STARTMONITOR);
+  LSW(STARTVACUUM) = mb.Hreg(16); MSW(STARTVACUUM) = mb.Hreg(17); EEPROM.put(116, STARTVACUUM);
+  LSW(STARTDILUTION) = mb.Hreg(18); MSW(STARTDILUTION) = mb.Hreg(19); EEPROM.put(120, STARTDILUTION);
+  LSW(STARTBYPASS) = mb.Hreg(20); MSW(STARTBYPASS) = mb.Hreg(21); EEPROM.put(124, STARTBYPASS);
+  LSW(STARTTEMP) = mb.Hreg(22); MSW(STARTTEMP) = mb.Hreg(23); EEPROM.put(128, STARTTEMP);
+  LSW(STARTELEMENTAL) = mb.Hreg(24); MSW(STARTELEMENTAL) = mb.Hreg(25); EEPROM.put(132, STARTELEMENTAL);
+  LSW(STARTERROR) = mb.Hreg(28); MSW(STARTERROR) = mb.Hreg(29); EEPROM.put(140, STARTERROR);
+  LSW(STARTCOEFF) = mb.Hreg(30); MSW(STARTCOEFF) = mb.Hreg(31); EEPROM.put(144, STARTCOEFF);
+}
+
+void startvalues(void)
+{
+  mb.Coil(0,false);//status of spectrometer
+  mb.Coil(1,true);//status of thermocontrollers
+  mb.Coil(2,false);//availability for external request
+  mb.Coil(3,false);//status of zero test
+  mb.Coil(4,false);//status of calibration
+  /*EEPROM.get(100, STARTCODE);
+  EEPROM.get(104, STARTTOTAL);
+  EEPROM.get(108, STARTOXIDIZED);
+  EEPROM.get(112, STARTMONITOR);
+  EEPROM.get(116, STARTVACUUM);
+  EEPROM.get(120, STARTDILUTION);
+  EEPROM.get(124, STARTBYPASS);
+  EEPROM.get(128, STARTTEMP);
+  EEPROM.get(132, STARTELEMENTAL);
+  EEPROM.get(140, STARTERROR);
+  EEPROM.get(144, STARTCOEFF);*/
+  mb.Hreg(8,LSW(STARTCODE));mb.Hreg(9,MSW(STARTCODE));//code of current mode
+  mb.Hreg(10,LSW(STARTTOTAL)); mb.Hreg(11,MSW(STARTTOTAL));//total
+  mb.Hreg(12,LSW(STARTOXIDIZED)); mb.Hreg(13,MSW(STARTOXIDIZED));//oxidized
+  mb.Hreg(14,LSW(STARTMONITOR)); mb.Hreg(15,MSW(STARTMONITOR));//monitor flow
+  mb.Hreg(16,LSW(STARTVACUUM)); mb.Hreg(17,MSW(STARTVACUUM));//vacuum
+  mb.Hreg(18,LSW(STARTDILUTION)); mb.Hreg(19,MSW(STARTDILUTION));//dilution pressure
+  mb.Hreg(20,LSW(STARTBYPASS)); mb.Hreg(21,MSW(STARTBYPASS));//bypass pressure
+  mb.Hreg(22,LSW(STARTTEMP)); mb.Hreg(23,MSW(STARTTEMP));//temp
+  mb.Hreg(24,LSW(STARTELEMENTAL)); mb.Hreg(25,MSW(STARTELEMENTAL));//elemental
+  mb.Hreg(26); mb.Hreg(27);//not used
+  mb.Hreg(28,LSW(STARTERROR)); mb.Hreg(29,MSW(STARTERROR));//errors and warnings
+  mb.Hreg(30,LSW(STARTCOEFF)); mb.Hreg(31,MSW(STARTCOEFF));//coefficient
+  mb.Hreg(32); mb.Hreg(33);//PMT current
+  mb.Hreg(40); mb.Hreg(41);//PMT voltage arb
+  mb.Hreg(42); mb.Hreg(43);//PMT voltage V
+  mb.Coil(99,false);//run calibration
+  mb.Coil(100,false);//run zero test
+  mb.Coil(101,false);//run elemental
+  mb.Coil(102,false);//start purge
+  mb.Coil(103,false);//end purge
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -22,29 +105,10 @@ void setup() {
   byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
   byte ip[] = { 192, 168, 1, 120 };
   mb.config(mac, ip);
-  mb.addCoil(0,false);//status of spectrometer
-  mb.addCoil(1,true);//status of thermocontrollers
-  mb.addCoil(2,false);//availability for external request
-  mb.addCoil(3,false);//status of zero test
-  mb.addCoil(4,false);//status of calibration
-  mb.addHreg(8);mb.addHreg(9);//code of current mode
-  mb.addHreg(10); mb.addHreg(11);//total
-  mb.addHreg(12); mb.addHreg(13);//oxidized
-  mb.addHreg(14); mb.addHreg(15);//monitor flow
-  mb.addHreg(16); mb.addHreg(17);//vacuum
-  mb.addHreg(18); mb.addHreg(19);//dilution pressure
-  mb.addHreg(20); mb.addHreg(21);//bypass pressure
-  mb.addHreg(22); mb.addHreg(23);//temp
-  mb.addHreg(24); mb.addHreg(25);//elemental
-  mb.addHreg(26); mb.addHreg(27);//not used
-  mb.addHreg(28); mb.addHreg(29);//errors and warnings
-  mb.addHreg(30); mb.addHreg(31);//coefficient
-  mb.addHreg(32); mb.addHreg(33);//lamp
-  mb.addCoil(99,false);//run calibration
-  mb.addCoil(100,false);//run zero test
-  mb.addCoil(101,false);//run elemental
-  mb.addCoil(102,false);//start purge
-  mb.addCoil(103,false);//end purge
+  setcoils(0,6);
+  sethregs(8,52);
+  setcoils(99,104);
+  startvalues();
 }
 
 void loop() {
