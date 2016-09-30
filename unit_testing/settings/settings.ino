9cc17ed8cc4b6d8cc6e28aa5cea28ca709876ee2
Art Navsegda <artnavsegda@gmail.com>
2016-09-30 07:13:11 +0300

    frozen

--- unit_testing/settings/settings.ino
+++ unit_testing/settings/settings.ino
@@ -4,12 +4,61 @@
 #define I2C_IPADDRESS 100
 #define I2C_MACADDRESS 101
 #define I2C_LENGTHTABLE 102
+#define I2C_JUMPTABLE 103
+
+enum modelist {
+  STARTLEVEL = 5,
+  CELLDELAY = 7,
+  CELLLEVEL = 8,
+  ZERODELAY = 11,
+  ZEROTEST = 12,
+  PURGE = 13,
+  TOTALMERCURYDELAY = 21,
+  TOTALMERCURY = 22,
+  ELEMENTALMERCURYDELAY = 26,
+  ELEMENTALMERCURY = 27,
+  PRECALIBRATIONDELAY = 31,
+  CALIBRATION = 32,
+  POSTCALIBRATIONDELAY = 33
+};
+
+struct lengthtablestruct {
+  int startlevel;
+  int celldelay;
+  int celllevel;
+  int zerodelay;
+  int zerotest;
+  int purge;
+  int totalmercurydelay;
+  int totalmercury;
+  int elementalmercurydelay;
+  int elementalmercury;
+  int precalibrationdelay;
+  int calibration;
+  int postcalibrationdelay;
+};
+
+struct jumptablestruct {
+  enum modelist startlevel;
+  enum modelist celldelay;
+  enum modelist celllevel;
+  enum modelist zerodelay;
+  enum modelist zerotest;
+  enum modelist purge;
+  enum modelist totalmercurydelay;
+  enum modelist totalmercury;
+  enum modelist elementalmercurydelay;
+  enum modelist elementalmercury;
+  enum modelist precalibrationdelay;
+  enum modelist calibration;
+  enum modelist postcalibrationdelay;
+};
 
 struct MyObject {
   byte ip[4];
   byte mac[6];
-  unsigned short length_table[13];
-  byte jump_table[13];
+  lengthtablestruct length_table;
+  jumptablestruct jump_table;
 };
 
 int memaddr;
@@ -37,7 +86,10 @@ void requestEvent() {
       Wire.write(e.mac,6);
     break;
     case I2C_LENGTHTABLE:
-      Wire.write((byte *)e.length_table,26);
+      Wire.write((byte *)&e.length_table,26);
+    break;
+    case I2C_JUMPTABLE:
+      Wire.write((byte *)&e.jump_table,26);
     break;
     default:
     break;
