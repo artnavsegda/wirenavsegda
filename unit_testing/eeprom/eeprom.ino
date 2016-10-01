63578e7c92684d986d5a8d6bb46cf0501492d84a
Art Navsegda <artnavsegda@gmail.com>
2016-10-01 08:42:45 +0300

    eeeah

--- unit_testing/eeprom/eeprom.ino
+++ unit_testing/eeprom/eeprom.ino
@@ -61,7 +61,7 @@ void setup() {
   // put your setup code here, to run once:
   EEPROM.get(0, e);
   Serial.begin(9600);
-  Serial.println("EEPROM test");
+  Serial.println("lentth table:");
   Serial.print("startlevel: ");Serial.println(e.length_table.startlevel);
   Serial.print("celldelay: ");Serial.println(e.length_table.celldelay);
   Serial.print("celllevel: ");Serial.println(e.length_table.celllevel);
@@ -75,6 +75,21 @@ void setup() {
   Serial.print("precalibrationdelay: ");Serial.println(e.length_table.precalibrationdelay);
   Serial.print("calibration: ");Serial.println(e.length_table.calibration);
   Serial.print("postcalibrationdelay: ");Serial.println(e.length_table.postcalibrationdelay);
+  
+  Serial.println("jump table:");
+  Serial.print("startlevel: ");Serial.println(e.jump_table.startlevel);
+  Serial.print("celldelay: ");Serial.println(e.jump_table.celldelay);
+  Serial.print("celllevel: ");Serial.println(e.jump_table.celllevel);
+  Serial.print("zerodelay: ");Serial.println(e.jump_table.zerodelay);
+  Serial.print("zerotest: ");Serial.println(e.jump_table.zerotest);
+  Serial.print("purge: ");Serial.println(e.jump_table.purge);
+  Serial.print("totalmercurydelay: ");Serial.println(e.jump_table.totalmercurydelay);
+  Serial.print("totalmercury: ");Serial.println(e.jump_table.totalmercury);
+  Serial.print("elementalmercurydelay: ");Serial.println(e.jump_table.elementalmercurydelay);
+  Serial.print("elementalmercury: ");Serial.println(e.jump_table.elementalmercury);
+  Serial.print("precalibrationdelay: ");Serial.println(e.jump_table.precalibrationdelay);
+  Serial.print("calibration: ");Serial.println(e.jump_table.calibration);
+  Serial.print("postcalibrationdelay: ");Serial.println(e.jump_table.postcalibrationdelay);
 }
 
 void loop() {
