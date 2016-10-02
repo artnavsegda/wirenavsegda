d1cbbaad3b354b766b403eb965288f2fe1915b16
Art Navsegda <artnavsegda@gmail.com>
2016-10-02 08:46:56 +0300

    add

--- unit_testing/adc/adc.ino
+++ unit_testing/adc/adc.ino
@@ -8,10 +8,10 @@ struct MyObject {
 };
 
 struct MyObject e = {
-  "1234",
-  "123456",
-  "abcdefghijklmnopqrstuwxyz",
-  "0123456789abc"
+  "123",
+  "12345",
+  "abcdefghijklmnopqrst",
+  "0123456789ab"
 };
 
 int memaddr;
@@ -19,10 +19,17 @@ int memaddr;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  Serial.println("ADC unit test");
   Wire.begin(8);
   Wire.onReceive(receiveEvent);
   Wire.onRequest(requestEvent);
 }
+
+int i;
+int x;
+float y;
+char z[5];
+uint16_t adc_scan_results[16];
 
 void receiveEvent(int howMany) {
   if (Wire.available())
@@ -30,23 +37,35 @@ void receiveEvent(int howMany) {
   if (Wire.available())
     switch (memaddr) {
       case 0:
-        Wire.readBytes(e.zero, 4);
-        Serial.println(e.zero);
-      break;
       case 1:
-        Wire.readBytes(e.one, 6);
-        Serial.println(e.one);
-      break;
       case 2:
-        Wire.readBytes(e.two, 26);
-        Serial.println(e.two);
-      break;
+      case 3:
       case 4:
-        Wire.readBytes(e.three, 13);
-        Serial.println(e.three);
+      case 5:
+      case 6:
+      case 7:
+      case 8:
+      case 9:
+      case 10:
+      case 11:
+      case 12:
+      case 13:
+      case 14:
+      case 15:
+        Wire.readBytes((byte *)&adc_scan_results[memaddr], 2);
+        Serial.print("ADC");
+        Serial.print(memaddr);
+        Serial.print(" ");
+        Serial.println(adc_scan_results[memaddr]);
+      break;
+      default:
       break;
     }
 }
+
+int xsend = 420;
+float ysend = 4.20;
+char zstring[] = "hello";
 
 void requestEvent() {
   switch (memaddr) {
@@ -57,10 +76,22 @@ void requestEvent() {
       Wire.write(e.one,6);
     break;
     case 2:
-      Wire.write(e.two,26);
+      Wire.write(e.two,20);
     break;
     case 3:
       Wire.write(e.three,13);
+    break;
+    case 5:
+      Wire.write(42);
+    break;
+    case 6:
+      Wire.write((byte *)&xsend,sizeof(xsend));
+    break;
+    case 7:
+      Wire.write((byte *)&ysend,sizeof(ysend));
+    break;
+    case 8:
+      Wire.write(zstring,sizeof(zstring));
     break;
     default:
     break;
