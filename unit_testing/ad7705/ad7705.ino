9d9ade130c246a8bcc634bdc4d72d100d62d2377
Art Navsegda <artnavsegda@gmail.com>
2016-10-04 21:32:24 +0300

    adcdata

--- unit_testing/ad7705/ad7705.ino
+++ unit_testing/ad7705/ad7705.ino
@@ -1,46 +1,25 @@
 #include <EEPROM.h>
 #include <Wire.h>
+#include <EtherCard.h>
+#include <Modbus.h>
+#include <ModbusIP_ENC28J60.h>
 #include "settings.h"
 
+ModbusIP mb;
 int memaddr;
+unsigned int adcdata;
 MyObject e;
 
 void setup() {
   // put your setup code here, to run once:
-  EEPROM.get(0, e);
-  Wire.begin(8);
   Serial.begin(9600);
+  Serial.println("AD7705 unit test");
+  Wire.begin(8);
   Wire.onReceive(receiveEvent);
   Wire.onRequest(requestEvent);
-  Serial.println("lentth table:");
-  Serial.print("startlevel: ");Serial.println(e.length_table.startlevel);
-  Serial.print("celldelay: ");Serial.println(e.length_table.celldelay);
-  Serial.print("celllevel: ");Serial.println(e.length_table.celllevel);
-  Serial.print("zerodelay: ");Serial.println(e.length_table.zerodelay);
-  Serial.print("zerotest: ");Serial.println(e.length_table.zerotest);
-  Serial.print("purge: ");Serial.println(e.length_table.purge);
-  Serial.print("totalmercurydelay: ");Serial.println(e.length_table.totalmercurydelay);
-  Serial.print("totalmercury: ");Serial.println(e.length_table.totalmercury);
-  Serial.print("elementalmercurydelay: ");Serial.println(e.length_table.elementalmercurydelay);
-  Serial.print("elementalmercury: ");Serial.println(e.length_table.elementalmercury);
-  Serial.print("precalibrationdelay: ");Serial.println(e.length_table.precalibrationdelay);
-  Serial.print("calibration: ");Serial.println(e.length_table.calibration);
-  Serial.print("postcalibrationdelay: ");Serial.println(e.length_table.postcalibrationdelay);
-  
-  Serial.println("jump table:");
-  Serial.print("startlevel: ");Serial.println(e.jump_table.startlevel);
-  Serial.print("celldelay: ");Serial.println(e.jump_table.celldelay);
-  Serial.print("celllevel: ");Serial.println(e.jump_table.celllevel);
-  Serial.print("zerodelay: ");Serial.println(e.jump_table.zerodelay);
-  Serial.print("zerotest: ");Serial.println(e.jump_table.zerotest);
-  Serial.print("purge: ");Serial.println(e.jump_table.purge);
-  Serial.print("totalmercurydelay: ");Serial.println(e.jump_table.totalmercurydelay);
-  Serial.print("totalmercury: ");Serial.println(e.jump_table.totalmercury);
-  Serial.print("elementalmercurydelay: ");Serial.println(e.jump_table.elementalmercurydelay);
-  Serial.print("elementalmercury: ");Serial.println(e.jump_table.elementalmercury);
-  Serial.print("precalibrationdelay: ");Serial.println(e.jump_table.precalibrationdelay);
-  Serial.print("calibration: ");Serial.println(e.jump_table.calibration);
-  Serial.print("postcalibrationdelay: ");Serial.println(e.jump_table.postcalibrationdelay);
+  EEPROM.get(0, e);
+  mb.config(e.mac, e.ip);
+  mb.addIreg(0);
 }
 
 void receiveEvent(int howMany) {
@@ -50,6 +29,7 @@ void receiveEvent(int howMany) {
     switch (memaddr) {
       case 0:
         Wire.readBytes((byte *)&adcdata, 2);
+        mb.Ireg(0,adcdata);
         Serial.print("AD7705");
         Serial.print(" ");
         Serial.println(adcdata);
