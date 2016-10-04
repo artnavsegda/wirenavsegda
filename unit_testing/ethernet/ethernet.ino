b2ce231260a51114610aa39d14435ea51294ef90
Art Navsegda <artnavsegda@gmail.com>
2016-10-04 15:10:13 +0300

    modbus

--- unit_testing/ethernet/ethernet.ino
+++ unit_testing/ethernet/ethernet.ino
@@ -1,12 +1,17 @@
 #include <EEPROM.h>
+#include <Wire.h>
 #include <EtherCard.h>
 #include "settings.h"
 
 byte Ethernet::buffer[300];   // a very small tcp/ip buffer is enough here
 MyObject e;
+int memaddr;
 
 void setup () {
   Serial.begin(9600);
+  Wire.begin(8);
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
   EEPROM.get(0, e);
   if (ether.begin(sizeof Ethernet::buffer, e.mac) == 0)
     Serial.println(F("Failed to access Ethernet controller"));
@@ -16,4 +21,22 @@ void setup () {
 
 void loop () {
   ether.packetLoop(ether.packetReceive());
+}
+
+void receiveEvent(int howMany) {
+  if (Wire.available())
+    memaddr = Wire.read();
+}
+
+void requestEvent() {
+  switch (memaddr) {
+    case I2C_IPADDRESS:
+      Wire.write(e.ip,4);
+    break;
+    case I2C_MACADDRESS:
+      Wire.write(e.mac,6);
+    break;
+    default:
+    break;
+  }
 }
--- unit_testing/modbus/modbus.ino
+++ unit_testing/modbus/modbus.ino
@@ -7,11 +7,32 @@ ModbusIP mb;
 MyObject e;
 
 void setup() {
+  Serial.begin(9600);
+  Wire.begin(8);
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
   EEPROM.get(0, e);
   mb.config(e.mac, e.ip);
 }
 
 void loop() {
-  //Call once inside loop() - all magic here
   mb.task();
+}
+
+void receiveEvent(int howMany) {
+  if (Wire.available())
+    memaddr = Wire.read();
+}
+
+void requestEvent() {
+  switch (memaddr) {
+    case I2C_IPADDRESS:
+      Wire.write(e.ip,4);
+    break;
+    case I2C_MACADDRESS:
+      Wire.write(e.mac,6);
+    break;
+    default:
+    break;
+  }
 }
