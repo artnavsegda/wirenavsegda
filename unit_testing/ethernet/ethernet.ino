6fa2e752859a0fc5f6b7c929001c5c4567fb18c2
Art Navsegda <artnavsegda@gmail.com>
2016-10-04 14:34:46 +0300

    testing

--- unit_testing/ethernet/ethernet.ino
+++ unit_testing/ethernet/ethernet.ino
@@ -6,10 +6,12 @@ byte Ethernet::buffer[300];   // a very small tcp/ip buffer is enough here
 MyObject e;
 
 void setup () {
+  Serial.begin(9600);
   EEPROM.get(0, e);
   if (ether.begin(sizeof Ethernet::buffer, e.mac) == 0)
     Serial.println(F("Failed to access Ethernet controller"));
   ether.staticSetup(e.ip);
+  ether.printIp("My IP: ", ether.myip);
 }
 
 void loop () {
