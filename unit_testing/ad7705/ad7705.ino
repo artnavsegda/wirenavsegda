61ccda089bb708e6803931b55d9d93313b2fc01b
Art Navsegda <artnavsegda@gmail.com>
2016-10-04 13:29:48 +0300

    settings

--- unit_testing/ad7705/ad7705.ino
+++ unit_testing/ad7705/ad7705.ino
@@ -1,35 +1,47 @@
+#include <EEPROM.h>
 #include <Wire.h>
-
-struct MyObject {
-  char zero[4];
-  char one[6];
-  char two[26];
-  char three[13];
-};
-
-struct MyObject e = {
-  "123",
-  "12345",
-  "abcdefghijklmnopqrst",
-  "0123456789ab"
-};
+#include "settings.h"
 
 int memaddr;
+MyObject e;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-  Serial.println("AD7705 unit test");
+  EEPROM.get(0, e);
   Wire.begin(8);
+  Serial.begin(9600);
   Wire.onReceive(receiveEvent);
   Wire.onRequest(requestEvent);
+  Serial.println("lentth table:");
+  Serial.print("startlevel: ");Serial.println(e.length_table.startlevel);
+  Serial.print("celldelay: ");Serial.println(e.length_table.celldelay);
+  Serial.print("celllevel: ");Serial.println(e.length_table.celllevel);
+  Serial.print("zerodelay: ");Serial.println(e.length_table.zerodelay);
+  Serial.print("zerotest: ");Serial.println(e.length_table.zerotest);
+  Serial.print("purge: ");Serial.println(e.length_table.purge);
+  Serial.print("totalmercurydelay: ");Serial.println(e.length_table.totalmercurydelay);
+  Serial.print("totalmercury: ");Serial.println(e.length_table.totalmercury);
+  Serial.print("elementalmercurydelay: ");Serial.println(e.length_table.elementalmercurydelay);
+  Serial.print("elementalmercury: ");Serial.println(e.length_table.elementalmercury);
+  Serial.print("precalibrationdelay: ");Serial.println(e.length_table.precalibrationdelay);
+  Serial.print("calibration: ");Serial.println(e.length_table.calibration);
+  Serial.print("postcalibrationdelay: ");Serial.println(e.length_table.postcalibrationdelay);
+  
+  Serial.println("jump table:");
+  Serial.print("startlevel: ");Serial.println(e.jump_table.startlevel);
+  Serial.print("celldelay: ");Serial.println(e.jump_table.celldelay);
+  Serial.print("celllevel: ");Serial.println(e.jump_table.celllevel);
+  Serial.print("zerodelay: ");Serial.println(e.jump_table.zerodelay);
+  Serial.print("zerotest: ");Serial.println(e.jump_table.zerotest);
+  Serial.print("purge: ");Serial.println(e.jump_table.purge);
+  Serial.print("totalmercurydelay: ");Serial.println(e.jump_table.totalmercurydelay);
+  Serial.print("totalmercury: ");Serial.println(e.jump_table.totalmercury);
+  Serial.print("elementalmercurydelay: ");Serial.println(e.jump_table.elementalmercurydelay);
+  Serial.print("elementalmercury: ");Serial.println(e.jump_table.elementalmercury);
+  Serial.print("precalibrationdelay: ");Serial.println(e.jump_table.precalibrationdelay);
+  Serial.print("calibration: ");Serial.println(e.jump_table.calibration);
+  Serial.print("postcalibrationdelay: ");Serial.println(e.jump_table.postcalibrationdelay);
 }
-
-int i;
-int x;
-float y;
-char z[5];
-uint16_t adcdata;
 
 void receiveEvent(int howMany) {
   if (Wire.available())
@@ -47,35 +59,25 @@ void receiveEvent(int howMany) {
     }
 }
 
-int xsend = 420;
-float ysend = 4.20;
-char zstring[] = "hello";
-
 void requestEvent() {
   switch (memaddr) {
-    case 0:
-      Wire.write(e.zero,4);
+    case I2C_IPADDRESS:
+      Wire.write(e.ip,4);
     break;
-    case 1:
-      Wire.write(e.one,6);
+    case I2C_MACADDRESS:
+      Wire.write(e.mac,6);
     break;
-    case 2:
-      Wire.write(e.two,20);
+    case I2C_LENGTHTABLE:
+      Wire.write((byte *)&e.length_table,26);
     break;
-    case 3:
-      Wire.write(e.three,13);
+    case I2C_JUMPTABLE:
+      Wire.write((byte *)&e.jump_table,13);
     break;
-    case 5:
-      Wire.write(42);
+    case I2C_AD7705_SETUP_REGISTER:
+      Wire.write(e.ad7705_setup_register);
     break;
-    case 6:
-      Wire.write((byte *)&xsend,sizeof(xsend));
-    break;
-    case 7:
-      Wire.write((byte *)&ysend,sizeof(ysend));
-    break;
-    case 8:
-      Wire.write(zstring,sizeof(zstring));
+    case I2C_AD7705_CLOCK_REGISTER:
+      Wire.write(e.ad7705_clock_register);
     break;
     default:
     break;
