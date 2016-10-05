32d50db368e7eea2a2c2ad44b9c93d74fe68ffc1
Art Navsegda <artnavsegda@gmail.com>
2016-10-05 14:23:42 +0300

    script

--- script2.sh
+++ script2.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ./set2eeprom < settings.txt > eeprom.raw
-/Applications/Arduino.app/Contents/Java/hardware/tools/avr/bin/avrdude -C /Applications/Arduino.app/Contents/Java/hardware/tools/avr/etc/avrdude.conf -c arduino -p atmega328p -P /dev/cu.usbserial-A400X396 -b 57600 -v -U eeprom:w:eeprom.raw:r
+/Applications/Arduino.app/Contents/Java/hardware/tools/avr/bin/avrdude -C /Applications/Arduino.app/Contents/Java/hardware/tools/avr/etc/avrdude.conf -c arduino -p atmega328p -P /dev/cu.usbserial-AH0384VD -b 57600 -v -U eeprom:w:eeprom.raw:r
 
