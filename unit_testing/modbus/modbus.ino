736aac9da07569770147d7b4e08b2c6f99a780a7
Art Navsegda <artnavsegda@gmail.com>
2016-10-24 03:44:05 +0300

    govnokod yebaniy

--- unit_testing/modbus/modbus.ino
+++ unit_testing/modbus/modbus.ino
@@ -7,7 +7,7 @@
 
 ModbusIP mb;
 MyObject myeeprom;
-int memaddr;
+uint8_t memaddr;
 
 void setup() {
   Serial.begin(9600);
@@ -26,56 +26,76 @@ void loop() {
   mb.task();
 }
 
+enum what_i2c_data_is_that(uint8_t i2caddr) {
+  if (rawmemchr(i2c_coils,i2caddr)!=NULL)
+    return MODBUS_COIL;
+  else if (rawmemchr(i2c_floats,i2caddr)!=NULL)
+    return MODBUS_FLOAT;
+  else
+    return MODBUS_UNKNOWN;
+}
+
+uint16_t i2c_address_to_modbus_number(uint8_t i2caddr) {
+  return modbus_position[i2caddr];
+}
+
+void write_double_modbus_registers(uint8_t i2caddr) {
+  float datatowrite;
+  Wire.readBytes((byte *)&datatowrite,sizeof(datatowrite));
+  uint16_t registertouse = i2c_address_to_modbus_number[i2caddr];
+  mb.Hreg(registertouse,LSW(datatowrite);
+  mb.Hreg(registertouse+1,MSW(datatowrite);
+}
+
 void receiveEvent(int howMany) {
   if (Wire.available())
     memaddr = Wire.read();
   if (Wire.available())
-    switch (memaddr) {
-      case STATUSOFSPECTROMETER: mb.Coil(myeeprom.modbustable.statusofspectrometer,Wire.read()); break;
-      case STATUSOFTHERMOCONTROLLERS: mb.Coil(myeeprom.modbustable.statusofthermocontrollers,Wire.read()); break;
-      case AVAILABILITYOFEXTERNALREQUEST: mb.Coil(myeeprom.modbustable.availabilityforexternalrequest,Wire.read()); break;
-      case STATUSOFZEROTEST: mb.Coil(myeeprom.modbustable.statusofzerotest,Wire.read()); break;
-      case STATUSOFCALIBRATION: mb.Coil(myeeprom.modbustable.statusofcalibration,Wire.read()); break;
-      case REQUESTTOSTARTCALIBRATION: mb.Coil(myeeprom.modbustable.runcalibration,Wire.read()); break;
-      case REQUESTTOSTARTZEROTEST: mb.Coil(myeeprom.modbustable.runzerotest,Wire.read()); break;
-      case REQUESTTOSTARTMEASURMENTOFELEMENTALMERCURY: mb.Coil(myeeprom.modbustable.runelemental,Wire.read()); break;
-      case REQUESTTOSTARTPURGE: mb.Coil(myeeprom.modbustable.startpurge,Wire.read()); break;
-      case REQUESTTOENDPURGE: mb.Coil(myeeprom.modbustable.endpurge,Wire.read()); break;
-      case 2:
-        Wire.readBytes((byte *)&x,sizeof(x));
-        Serial.println(x);
-        mb.Ireg(6,x);
-       break;
-      case 3:
-        Wire.readBytes((byte *)&y,sizeof(y));
-        Serial.println(y);
+  {
+    switch (what_i2c_data_is_that(memaddr)) {
+      case MODBUS_COIL:
+        mb.Coil(i2c_address_to_modbus_number(memaddr),Wire.read());
       break;
-      case 4:
-        Wire.readBytes(z,sizeof(z));
-        Serial.println(z);
+      case MODBUS_FLOAT:
+        write_double_modbus_registers(memaddr);
       default:
       break;
     }
+  }
 }
 
 void requestEvent() {
   switch (memaddr) {
-    case I2C_IPADDRESS: Wire.write(myeeprom.ip,4); break;
-    case I2C_MACADDRESS: Wire.write(myeeprom.mac,6); break;
-    case I2C_LENGTHTABLE: Wire.write((byte *)&myeeprom.length_table,26); break;
-    case I2C_JUMPTABLE: Wire.write((byte *)&myeeprom.jump_table,13); break;
-    case I2C_AD7705_SETUP_REGISTER: Wire.write(myeeprom.ad7705_setup_register); break;
-    case I2C_AD7705_CLOCK_REGISTER: Wire.write(myeeprom.ad7705_clock_register); break;
-    case STATUSOFSPECTROMETER: Wire.write(mb.Coil(myeeprom.modbustable.statusofspectrometer); break;
-    case STATUSOFTHERMOCONTROLLERS: Wire.write(mb.Coil(myeeprom.modbustable.statusofthermocontrollers); break;
-    case AVAILABILITYOFEXTERNALREQUEST: Wire.write(mb.Coil(myeeprom.modbustable.availabilityforexternalrequest); break;
-    case STATUSOFZEROTEST: Wire.write(mb.Coil(myeeprom.modbustable.statusofzerotest); break;
-    case STATUSOFCALIBRATION: Wire.write(mb.Coil(myeeprom.modbustable.statusofcalibration); break;
-    case REQUESTTOSTARTCALIBRATION: Wire.write(mb.Coil(myeeprom.modbustable.runcalibration); break;
-    case REQUESTTOSTARTZEROTEST: Wire.write(mb.Coil(myeeprom.modbustable.runzerotest); break;
-    case REQUESTTOSTARTMEASURMENTOFELEMENTALMERCURY: Wire.write(mb.Coil(myeeprom.modbustable.runelemental); break;
-    case REQUESTTOSTARTPURGE: Wire.write(mb.Coil(myeeprom.modbustable.startpurge); break;
-    case REQUESTTOENDPURGE:Wire.write(mb.Coil(myeeprom.modbustable.endpurge); break;
+    case I2C_IPADDRESS:
+      Wire.write(myeeprom.ip,4);
+    break;
+    case I2C_MACADDRESS:
+      Wire.write(myeeprom.mac,6);
+    break;
+    case I2C_LENGTHTABLE:
+      Wire.write((byte *)&myeeprom.length_table,26);
+    break;
+    case I2C_JUMPTABLE:
+      Wire.write((byte *)&myeeprom.jump_table,13);
+    break;
+    case I2C_AD7705_SETUP_REGISTER:
+      Wire.write(myeeprom.ad7705_setup_register);
+    break;
+    case I2C_AD7705_CLOCK_REGISTER:
+      Wire.write(myeeprom.ad7705_clock_register);
+    break;
+    case STATUSOFSPECTROMETER:
+    case STATUSOFTHERMOCONTROLLERS:
+    case AVAILABILITYOFEXTERNALREQUEST:
+    case STATUSOFZEROTEST:
+    case STATUSOFCALIBRATION:
+    case REQUESTTOSTARTCALIBRATION:
+    case REQUESTTOSTARTZEROTEST:
+    case REQUESTTOSTARTMEASURMENTOFELEMENTALMERCURY:
+    case REQUESTTOSTARTPURGE:
+    case REQUESTTOENDPURGE:
+      Wire.write(mb.Coil(myeeprom.modbustable.endpurge));
+    break;
     default:
     break;
   }
